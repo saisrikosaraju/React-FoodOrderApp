e898d460476412f1ba65600f1175cd23a005b2e4
Sai Sri kosaraju <koar.sail@gmail.com>
2022-01-19 13:14:47 +0100

    Update MealItemForm.js

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -10,8 +10,10 @@ const MealItemForm = (props)=>{
     const submitHandler =(event)=>{
         event.preventDefault();
         const enteredAmount = amountInputRef.current.value;
-        const enteredAmountNumber = +enteredAmount;
+        const enteredAmountNumber = +enteredAmount; //converting amount to number
 
+        
+        //validate the entered amount by checking min max values
         if(enteredAmount.trim().length === 0 || 
         enteredAmountNumber < 1 ||
         enteredAmountNumber > 5){
